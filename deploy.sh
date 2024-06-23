84999928a8b1a5c5bc432565cc0d1d5667d18cd0
Vamist <dm@vamist.dev>
2024-06-23 12:13:00 +0100

    [doc] Expanded the comment on deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo $1
 echo $2
-# Password does not matter
+# If you have set up usbnetwork and not touched the settings, you can put whatever gibberish you want
+# The kindle will accept any connection, but it still wants a password
 sshpass -p 'alwhndjkawhdkhjawd' scp $1 root@$2:/mnt/us/documents/
