66730146e4b2bbdfba3e13c39eadb21bb7460e17
Vamist <dm@vamist.dev>
2024-06-23 14:00:26 +0100

    [dev] Format

--- src/com/test/app/Main.java
+++ src/com/test/app/Main.java
@@ -4,14 +4,11 @@ import com.amazon.kindle.kindlet.KindletContext;
 import com.amazon.kindle.kindlet.ui.KTextArea;
 import com.cowlark.kindlet.KindletWrapper;
 
-public class Main extends KindletWrapper
-{
-	@Override
-	public void onKindletStart()
-	{
-    	KindletContext context = getContext();
-		KTextArea kta = new KTextArea("Hello, world!");
-		context.getRootContainer().add(kta);
-	}
+public class Main extends KindletWrapper {
+  @Override
+  public void onKindletStart() {
+    KindletContext context = getContext();
+    KTextArea kta = new KTextArea("Hello, world!");
+    context.getRootContainer().add(kta);
+  }
 }
-
